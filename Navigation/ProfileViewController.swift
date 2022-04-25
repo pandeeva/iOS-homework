443ae682d0a8cd2c28f831c175ac0aabcae76d7b
pandeeva <96229764+pandeeva@users.noreply.github.com>
2022-04-26 02:40:00 +0300

    Добавлена вторая домашка

--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ProfileViewController.swift
 //  Navigation
 //
 //  Created by Бардеева Надежда on 26.04.2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
     }
 
-
 }
-
